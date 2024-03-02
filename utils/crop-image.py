869d7f837a9a275ca3e4a8fc72b024b8cce7bdb1
yigalk89 <48278992+yigalk89@users.noreply.github.com>
2024-03-02 20:40:18 +0200

    Update crop-image.py

--- utils/crop-image.py
+++ utils/crop-image.py
@@ -3,6 +3,7 @@ import imageio
 import numpy as np
 from PIL import Image, ImageFile
 import os
+import argparse
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def imread(path):
@@ -28,15 +29,15 @@ def parse_minute_file(mnt_file_path):
     return mnt
 
 
-def main():
-    base_path = '/home/zev/projects/OAI/fingerprint-generator-project/fingerprint_dataset'
+def main(dataset="train", base_path):
+    # base_path = '/home/zev/projects/OAI/fingerprint-generator-project/fingerprint_dataset'
     i = 0
     corrupted = 0
-    for file in os.listdir(base_path + 'test/'):
+    for file in os.listdir(base_path + f'{dataset}/'):
         # print(file)
-        image = imread(base_path + 'test/' + file)
+        image = imread(base_path + f'{dataset}/' + file)
         w,h = image.size
-        mnt = parse_minute_file(base_path + 'test_mintxt/' + file[:-4] + '.txt')
+        mnt = parse_minute_file(base_path + f'{dataset}_mintxt/' + file[:-4] + '.txt')
         if mnt.size == 0:
             corrupted += 1
             continue
@@ -56,11 +57,15 @@ def main():
         left, top, right, bottom = (min_w, min_h, max_w, max_h)
         new_image = get_image(image, left, top, right, bottom)
 
-        imageio.imsave(base_path + 'test_cropped/cropped-' + file[:-4] + '.png', new_image)
+        imageio.imsave(base_path + f'{dataset}_cropped/cropped-' + file[:-4] + '.png', new_image)
         i += 1
         if (i % 1000 == 0):
             print('Hit', i, 'images with', corrupted, 'corrupted images')
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Train or Test')
+    parser.add_argument('-d', '--dataset') 
+    parser.add_argument('-p', '--base_path')
+    args = parser.parse_args()
+    main(args.dataset, args.base_path)
