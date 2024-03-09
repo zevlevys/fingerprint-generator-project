5ad89980def70d4320a563349d83bcce1c496ddb
yigalk89 <48278992+yigalk89@users.noreply.github.com>
2024-03-09 23:27:26 +0200

    Update inference_mnt_encoder.py

--- scripts/inference_mnt_encoder.py
+++ scripts/inference_mnt_encoder.py
@@ -23,15 +23,15 @@ def combine_images(columns, space, images, save_path):
     rows = len(images) // columns
     if len(images) % columns:
         rows += 1
-    width_max = max([Image.open(image).width for image in images])
-    height_max = max([Image.open(image).height for image in images])
+    width_max = max([image.width for image in images])
+    height_max = max([image.height for image in images])
     background_width = width_max*columns + (space*columns)-space
     background_height = height_max*rows + (space*rows)-space
     background = Image.new('RGBA', (background_width, background_height), (255, 255, 255, 255))
     x = 0
     y = 0
     for i, image in enumerate(images):
-        img = Image.open(image)
+        img = image
         x_offset = int((width_max-img.width)/2)
         y_offset = int((height_max-img.height)/2)
         background.paste(img, (x+x_offset, y+y_offset))
